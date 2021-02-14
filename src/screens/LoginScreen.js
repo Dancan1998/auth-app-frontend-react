42aec5c9bcc05fe0946cffa0cabe549767bb3ebb
Dancan Chibole <dancankingstar@gmail.com>
2021-02-14 10:30:57 -0800

    Created a fform

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import FormContainer from "../components/FormContainer";
 import { Form, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const LoginScreen = () => {
+const LoginScreen = ({ location, history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("login");
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <FormContainer>
+      <h1>Log In</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email">
+          <Form.Label>Email Adress</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="dark">
+          Log In
+        </Button>
+      </Form>
+
+      <Row className="py-3">
+        <Col>
+          <Link
+            className="btn btn-dark"
+            to={redirect ? `/register?redirect=${redirect}` : "/register"}
+          >
+            Register
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
   );
 };
 
